82af7a87375cd1c5556d4263329a09be1721779a
surrealdetective <thefifth@gmail.com>
2019-08-08 15:54:22 -0700

    Switch to postgres on dev db

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,5 +1,6 @@
 class Worker < ApplicationRecord
   validates_presence_of :name, :company_name, :email
+  validates_uniqueness_of :email
   has_many :work_orders_workers
   has_many :work_orders, :through => :work_orders_workers
 end
