d721e836a8326516a9e5d5264020abae0200d35a
surrealdetective <thefifth@gmail.com>
2019-08-08 14:45:11 -0700

    Seed db to play around w/ manually

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+worker = Worker.new(name: 'Joseph Joestar', company_name: "Bizarre Adventures", email: 'jojo@bizarre.com' )
+worker.save
+10.times do |n|
+  worker.work_orders.create(title: Faker::Lorem.word, description: Faker::Lorem.word, deadline: DateTime.new(2030, 10, n+1))
+end
+
+4.times do
+  worker = Worker.new(name: Faker::Lorem.word, company_name: Faker::Lorem.word, email: Faker::Internet.email )
+  worker.save
+  10.times do |n|
+    worker.work_orders.create(title: Faker::Lorem.word, description: Faker::Lorem.word, deadline: DateTime.new(2030, 10, n+1))
+  end
+end
